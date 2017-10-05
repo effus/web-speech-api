2b1ee0450305a6af9c4d50a6338cca4b98caa1de
effus <effusps@gmail.com>
2017-10-05 22:05:32 +0300

    game js

--- demo/ping-pong/assets/game.js
+++ demo/ping-pong/assets/game.js
@@ -1,5 +1,38 @@
 var Game = {
+  started: false,
+  paused: false,
+  difficulty: 2,
+  padDirection: 0,
+  padSpeed: 5,
   init: function() {
-
+    PPong.controllers.getActive().resumeCallback = function() {
+      if (Game.started) {
+        Render.resume();
+        Game.paused = false;
+      }
+    };
+  },
+  start: function() {
+    if (!this.started) {
+      Render.construct();
+      Render.randomizeOnStart();
+      this.started = true;
+      this.paused = false;
+    }
+  },
+  stop: function() {
+    if (this.started) {
+      Render.destruct();
+      this.started = false;
+      this.paused = false;
+    }
+  },
+  scores: {
+    left: 0,
+    right: 0
+  },
+  goal: function(winnerSide) {
+    Game.scores[winnerSide]++;
+    Game.paused = true;
   }
 };
